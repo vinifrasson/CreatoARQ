28bf65739d480ef49519f6b8a8f9dda9e94881d5
Vinícius Frasson <vinifrasson13@gmail.com>
2022-10-12 17:40:53 -0300

    CreatoARQ

---
+++ Creato/README.md
@@ -0,0 +1,6 @@
+# AeroPar
+Landing-Page Aeropar
+
+https://aeropar.com.py/
+
+![image](https://user-images.githubusercontent.com/84817988/182217691-073e726b-01fc-4a36-a036-591c5ddb419a.png)
---
+++ Creato/js/script.js
@@ -0,0 +1,55 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#f4f4f4';
+	} else {
+		header.style.backgroundColor = '#f4f4f4';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
